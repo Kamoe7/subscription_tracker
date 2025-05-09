d8b685ca3a5999546922b493711c1cee0e5bd15e
Susleen <114741775+Kamoe7@users.noreply.github.com>
2025-05-10 00:46:37 +0800

    Added authorization and arcjet to avoid spam

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import authRoutes  from "./routes/auth_routes.js";
 import subscriptionRoutes from "./routes/subscription_routes.js";
 import userRoutes from "./routes/user_routes.js";
 import connectToDatabase from "./Database/mongodb.js";
-import errorMiddleware from "./middlewares/error.middleware.js";
-import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js"
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 
 const app =express();
@@ -16,6 +16,7 @@ const app =express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 //app.use(cookieParser);
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/users',userRoutes);
